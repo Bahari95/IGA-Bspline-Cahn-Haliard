2e7af036b7cc01bb9cf228c64f3a266275aee002
Bahari Mustapha <75118127+Bahari95@users.noreply.github.com>
2023-03-05 21:51:19 +0100

    Add files via upload

---
+++ generalized_alpha_method/gallery_section_00.py
@@ -0,0 +1,10 @@
+__all__ = ['assemble_stiffnessmatrix1D',
+           'assemble_massmatrix1D',
+           'assemble_vector_ex01',
+]
+
+#==============================================================================Assembles stiffness matrix
+#---1 : In uniform mesh
+from pyccel.decorators import types
+
+
